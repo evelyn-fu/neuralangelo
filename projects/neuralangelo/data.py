fix masking

projects/neuralangelo/data.py
@@ -113,8 +113,7 @@ class Dataset(base.Dataset):
             fpath = self.list[idx]["file_path"]
             image_fname = f"{self.root}/{fpath}"
             image = Image.open(image_fname)
-            mask = np.ones(image.size)
-            mask.load()
+            mask = Image.fromarray(np.zeros(image.size))
             mask_size_raw = mask.size
             return mask, mask_size_raw
         else:
@@ -136,6 +135,7 @@ class Dataset(base.Dataset):
         # Resize the mask.
         mask = mask.resize((self.W, self.H))
         mask = torchvision_F.to_tensor(mask)
+        mask = 1 - mask # invert gripper masks
         return mask
 
     def get_camera(self, idx):

projects/neuralangelo/trainer.py
@@ -44,7 +44,7 @@ class Trainer(BaseTrainer):
             background_color = torch.rand_like(data["rgb"])
             rgb, opacity = data["image_sampled"][..., :3], data["image_sampled"][..., 3:]
             gt_image = rgb * opacity + background_color.to(rgb.device) * (1 - opacity)
-            pred_image = data["rgb"] + background_color * (1.0 - data["opacity"])
+            pred_image = data["rgb"] * data["opacity"] + background_color.to(data["rgb"].device) * (1.0 - data["opacity"])
 
             # Compute loss only on randomly sampled rays.
             self.losses["render"] = self.criteria["render"](pred_image, gt_image) * 3  # FIXME:sumRGB?!
@@ -59,7 +59,7 @@ class Trainer(BaseTrainer):
             background_color = torch.rand_like(data["rgb_map"])
             rgb, opacity = data["image"][:, :3, :, :], data["image"][:, 3:, :, :]
             gt_image = rgb * opacity + background_color.to(rgb.device) * (1 - opacity)
-            pred_image = data["rgb_map"] + background_color * (1.0 - data["opacity_map"])
+            pred_image = data["rgb_map"] * data["opacity_map"] + background_color.to(data["rgb_map"].device) * (1.0 - data["opacity_map"])
 
             # In inference mode, compute loss on the entire image.
             mask = data["mask"].bool().expand(-1, 3, -1, -1)  # Mask for the entire image if needed